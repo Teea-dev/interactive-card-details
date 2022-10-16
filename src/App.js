045bdf909a9e3d0963572b51212e57cf65c6cc09
Ogunnoiki Adetokunbo <adetokunboogunnoiki28@gmail.com>
2022-10-16 17:02:50 +0100

    worked on the file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import logo from "./images/card-logo.svg";
 import mobilebackground from "./images/bg-main-mobile.png";
 import cardfront from "./images/bg-card-front.png";
 import cardback from "./images/bg-card-back.png";
 import desktopbackground from "./images/bg-main-desktop.png";
-import { useForm } from "react-hook-form";
-import Details from "./Details";
+
+import Details from "./components/Details/Details";
+import Form from "./components/Form/Form";
 
 import "./App.css";
 
@@ -16,38 +16,12 @@ function App() {
   const [cvc, setCvc] = useState("000");
   const [submitted, setSubmitted] = useState(false);
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    switch (e.target.name) {
-      case "name":
-        setName(e.target.value);
-        break;
-      case "number":
-        setNumber(e.target.value);
-        break;
-      case "date":
-        setDate(e.target.value);
-        break;
-      case "cvc":
-        setCvc(e.target.value);
-        break;
-      default:
-        break;
-    }
-  };
-  
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
   return (
     <div className="container-fluid ">
       <div className="row">
-        <div className="col-sm-6">
-          <div className="row">
-            <div className=" col-sm-6 top">
+        <div className="top col-md-6">
+          {/* <div className="row"> */}
+            {/* <div className=" col-sm-6 top">
               <img
                 className="desktop d-none d-md-block"
                 src={desktopbackground}
@@ -58,10 +32,11 @@ function App() {
                 src={mobilebackground}
                 alt=""
               />
-            </div>
+            </div> */}
 
-            <div className=" col-sm-6 card-container ">
-              {/* <img src={logo} width='30px' /> */}
+            {/* <div className="  col-sm-6 card-container "> */}
+              <div className='wrapper'>
+
               <img src={cardfront} className="cardfront" alt="" width="300px" />
               <div className="card-num">{number}</div>
               <span className="card-name">
@@ -75,88 +50,28 @@ function App() {
               <span className="cvc">
                 <h3>{cvc}</h3>
               </span>
-            </div>
-          </div>
+              </div>
+            {/* </div> */}
+          {/* </div> */}
         </div>
-        <div className=" col-sm-6 details-section">
+        <div className=" col-md-6 details-section">
           <div className="login-section">
-              {submitted ?(
-                <Details/> 
-              ): 
+            {submitted ? (
+              <Details />
+            ) : (
               <>
-            <form
-              onSubmit={handleSubmit((e) => {
-                // e.preventDefault();
-                setSubmitted(true);
-              })}
-            >
-              <div className="input">
-                <label for="email"> cardholder name</label>
-                <input
-                  {...register("name")}
-                  className="input-field"
-                  type="text"
-                  name="name"
-                  id="email"
-                  // value={forValues.email}
-                  placeholder="e.g Jane Appleseed"
-                  onChange={handleChange}
+                <Form
+                  setCvc={setCvc}
+                  setDate={setDate}
+                  setNumber={setNumber}
+                  setName={setName}
+                  setSubmitted={setSubmitted}
                 />
-              </div>
-              <div className="input">
-                <label for="password"> card number</label>
-                <input
-                  {...register("number", {
-                    pattern: {
-                      value: /^[0-9]+$/,
-                      message: "Wrong format. numbers only",
-                    }
-                  })}
-                  className="input-field"
-                  type="text"
-                  name="number"
-                  id="password"
-                  // value={forValues.password}
-                  placeholder="e.g 1234 5678 9123 0000"
-                  onChange={handleChange}
-                />
-                <p className="error-messages">{errors.number?.message}</p>
-              </div>
-              <div className="  d-flex">
-                <div className=" ">
-                  <label> exp. date(mm/yy)</label> <br />
-                  <input
-                    {...register("date", { required: `can't be blank` })}
-                    type="month"
-                    className=""
-                    name="date"
-                    onChange={handleChange}
-                  />
-                  <p className="error-messages">{errors.date?.message}</p>
-                </div>
-                <div className="cvc-input ">
-                  <label> cvc</label> <br />
-                  <input
-                    {...register("cvc", { required: `can't be blank` })}
-                    type="year"
-                    name="cvc"
-                    className="input-cvc"
-                    onChange={handleChange}
-                  />
-                  <p className="error-messages">{errors.cvc?.message}</p>
-                </div>
-              </div>
-
-              <button type="submit" className="button">
-                <h3>Confirm</h3>
-              </button>
-            </form>
               </>
-              }
+            )}
           </div>
         </div>
       </div>
-       
     </div>
   );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const Form = (props) => {
+  const handleChange = (e) => {
+    e.preventDefault();
+    switch (e.target.name) {
+      case "name":
+        props.setName(e.target.value);
+        break;
+      case "number":
+        props.setNumber(e.target.value);
+        break;
+      case "date":
+        props.setDate(e.target.value);
+        break;
+      case "cvc":
+        props.setCvc(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit((e) => {
+          // e.preventDefault();
+          props.setSubmitted(true);
+        })}
+      >
+        <div className="input">
+          <label for="email"> cardholder name</label>
+          <input
+            {...register("name", { required: `Can't be blank` })}
+            className="input-field"
+            type="text"
+            name="name"
+            id="email"
+            // value={forValues.email}
+            placeholder="e.g Jane Appleseed"
+            onChange={handleChange}
+          />
+          <p className="error-messages"> {errors.name?.message}</p>
+        </div>
+        <div className="input">
+          <label for="password"> card number</label>
+          <input
+            {...register("number", {
+              required: true,
+              maxLength: 16,
+
+              pattern: {
+                value: /^[0-9]+$/,
+                message: "Wrong format. numbers only",
+              },
+            })}
+            className="input-field"
+            type="number"
+            name="number"
+            id="password"
+            // value={forValues.password}
+            placeholder="e.g 1234 5678 9123 0000"
+            onChange={handleChange}
+          />
+          {errors.number && errors.number.type === "required" && (
+            <p className="error-messages">can't be blank</p>
+          )}
+          {errors.number && errors.number.type === "maxLength" && (
+            <p className="error-messages">maximum length exceeded</p>
+          )}
+          <p className="error-messages">{errors.number?.message}</p>
+        </div>
+        <div className="  d-flex">
+          <div className=" ">
+            <label> exp. date(mm/yy)</label> <br />
+            <input
+              {...register("date", { required: `can't be blank` })}
+              type="month"
+              className=""
+              name="date"
+              onChange={handleChange}
+            />
+            <p className="error-messages">{errors.date?.message}</p>
+          </div>
+          <div className="cvc-input ">
+            <label> cvc</label> <br />
+            <input
+              {...register("cvc", { required: true, maxLength: 3 })}
+              type="year"
+              name="cvc"
+              className="input-cvc"
+              onChange={handleChange}
+            />
+            {errors.cvc && errors.cvc.type === "required" && (
+              <p className="error-messages">can't be blank</p>
+            )}
+            {errors.cvc && errors.cvc.type === "maxLength" && (
+              <p className="error-messages">maximum length exceeded</p>
+            )}
+          </div>
+        </div>
+
+        <button type="submit" className="button">
+          <h3>Confirm</h3>
+        </button>
+      </form>
+    </>
+  );
+};
+export default Form;
